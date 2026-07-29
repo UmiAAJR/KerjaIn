dd1c52fb4853be4a70a02a37e24846c94ec1a39f
Arjuna Marcel <arjunamarcel25@gmail.com>
2026-07-29 14:38:50 +0700

    feat: Adding separate job completion controller

--- backend/route/JobRoute.js
+++ backend/route/JobRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createJob, deleteJob, getJob, getDetailJob, updateJob } from '../controller/JobController.js'
+import { createJob, deleteJob, getJob, getDetailJob, updateJob, completeJob } from '../controller/JobController.js'
 import { CheckRole, VerifyUser } from '../middleware/UserAuth.js'
 
 const JobRoute = express()
@@ -7,6 +7,7 @@ const JobRoute = express()
 JobRoute.get("/", getJob)
 JobRoute.get("/:id", getDetailJob)
 JobRoute.post("/", VerifyUser, CheckRole(["admin"]), createJob)
+JobRoute.post("/complete/:id", VerifyUser, completeJob)
 JobRoute.patch("/:id", VerifyUser, CheckRole(["admin"]), updateJob)
 JobRoute.delete("/:id", VerifyUser, CheckRole(["admin"]), deleteJob)
 
